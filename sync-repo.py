6ed6b14de0213f92739d07fc5d80232303fadf74
David Kretzmer <david.k@posteo.de>
2023-10-26 15:15:19 +0200

    Remove {start|finish}_operation(), use unique bundle names instead

--- sync-repo.py
+++ sync-repo.py
@@ -4,12 +4,12 @@ import tempfile
 import json
 import subprocess
 import re
+import secrets
+import time
 from pathlib import Path
 
 
 NOTESYNC_DIR = ".notesync"
-ONGOING_OPERATION_FILE = "ongoing_operation"
-
 BUCKET_NAME = "notes-1234"
 
 # Utils
@@ -28,7 +28,19 @@ def make_bundle_name(instance_name: str, bundle_no: int):
     if "." in instance_name:
         raise RuntimeError("Instance name must not contain '.'")
 
-    return f"{bundle_no:05d}.{instance_name}.bundle"
+    # Add the current time and some random bytes to ensure that the generated bundle name is unique.
+    # This is needed because:
+    # - Bundles on the server must never change after they have been uploaded (because each bundle
+    #   contains the delta to the previous bundle, so changing a bundle may invalidate all following
+    #   bundles)
+    # - We upload new bundles without checking if a bundle with that name already exists.
+    #
+    # By including the instance name, the current time, and some random bytes in the bundle name,
+    # the chances of a name collision seem reasonably small.
+    timestamp = time.time_ns() // 10**6 # Current timestamp in milliseconds
+    random_str = secrets.token_hex(6)
+
+    return f"{bundle_no:05d}.{instance_name}.{timestamp}.{random_str}.bundle"
 
 def extract_bundle_info(bundle_name: str):
     parts = bundle_name.split(".")
@@ -83,18 +95,6 @@ def write_downloaded_bundle_no(bundle_no, repo_dir = "."):
         f.write(str(bundle_no))
 
 
-def start_operation(op_type, latest_uploaded_commit, downloaded_bundle_no, repo_dir = "."):
-    try:
-        with open(os.path.join(repo_dir, NOTESYNC_DIR, ONGOING_OPERATION_FILE), "x") as file:
-            file.write(op_type + " " + (latest_uploaded_commit or "-") + " " + str(downloaded_bundle_no))
-
-    except FileExistsError:
-        raise RuntimeError("A previous operation crashed or was terminated prematurely")
-
-def finish_operation(repo_dir = "."):
-    Path.unlink(os.path.join(repo_dir, NOTESYNC_DIR, ONGOING_OPERATION_FILE))
-
-
 # Pulling bundles
 #-------------------------------------------------------------------------------
 def pull_from_remote(remote_bundle):
@@ -116,25 +116,21 @@ def pull_from_remote(remote_bundle):
             stdin=passphrase
         )
 
-        run_command(["git", "bundle", "verify", bundle_filename])
-
-        downloaded_bundle_no = read_downloaded_bundle_no()
-        uploaded_commit_id = read_uploaded_commit_id()
-        start_operation("pull", uploaded_commit_id, downloaded_bundle_no)
-
         # Pull from bundle
+        run_command(["git", "bundle", "verify", bundle_filename])
         run_command(["git", "pull", "--rebase", bundle_filename])
 
         # If the bundle contains a more recent commit than what we have uploaded, then update the uploaded commit id.
         # The ancestor check shouldn't actually be needed since we are only ever pulling newer bundles.
+        # - Actually, the check is necessary if a bundle was pulled but the script crashed before it
+        #   could update the uploaded commit id
+        uploaded_commit_id = read_uploaded_commit_id()
         bundle_commit = get_master_commit_from_bundle(bundle_filename)
         if uploaded_commit_id == None or is_first_commit_ancestor_of_second(uploaded_commit_id, bundle_commit):
             write_uploaded_commit_id(bundle_commit)
 
         new_bundle_no, _ = extract_bundle_info(remote_bundle["fileName"])
         write_downloaded_bundle_no(new_bundle_no)
-
-        finish_operation()
 
 
 def b2_fileinfo_to_tuple(fileinfo):
@@ -187,14 +183,12 @@ def command_push(repo_dir, instance_name):
             os.chdir(repo_dir)
 
             current_commit_id = run_command(["git", "rev-parse", "master"]).stdout.splitlines()[0]
-            downloaded_bundle_no = read_downloaded_bundle_no()
             uploaded_commit_id = read_uploaded_commit_id()
-
             if uploaded_commit_id == current_commit_id:
                 return
 
             # Construct bundle name to upload
-            new_bundle_no = downloaded_bundle_no + 1
+            new_bundle_no = read_downloaded_bundle_no() + 1
             bundle_name = make_bundle_name(instance_name, new_bundle_no)
             bundle_filename = os.path.join(temp_dir, bundle_name)
 
@@ -214,20 +208,16 @@ def command_push(repo_dir, instance_name):
                 stdin = read_file(os.path.join(repo_dir, ".passphrase"))
             )
 
-            start_operation("push", uploaded_commit_id, downloaded_bundle_no)
-
             # Upload encrypted bundle to a Backblaze B2 bucket
             run_command(["backblaze-b2", "upload_file", BUCKET_NAME, enc_bundle_filename, enc_bundle_name])
 
             if check_for_collision(instance_name, new_bundle_no):
                 delete_uploaded_file(enc_bundle_name)
-                finish_operation()
                 raise RuntimeError("New data available. Please pull and then push again.")
 
             # Remember the current commit so we that next time we know not to upload it again (saves bandwidth)
             write_uploaded_commit_id(current_commit_id)
             write_downloaded_bundle_no(new_bundle_no)
-            finish_operation()
 
             run_command(["notify-send", "Notes: Upload successful"])
 
